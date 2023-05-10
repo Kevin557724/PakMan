ede5c62a53ff871256fb19aeb7da8344cda2e19c
Kevin <Kesalvador@s.sfusd.edu>
2023-05-10 10:32:17 -0700

    No changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1.0f;
+    public float RotateSpeed = 30.0f;
     private Rigidbody playerRb;
     private Rigidbody rb;
+     
 
     // Start is called before the first frame update
     void Start()
@@ -14,9 +16,12 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
            float moveHorizontal = Input.GetAxis ("Horizontal");
      float moveVertical = Input.GetAxis ("Vertical");
-     Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+     Vector3 movement = new Vector3 (moveHorizontal, moveVertical);
      rb.AddForce (movement);
-         
+           if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
     }
 
     // Update is called once per frame
@@ -27,5 +32,7 @@ public class PlayerController : MonoBehaviour
        
          playerRb.AddForce(Vector3.forward *speed * verticalInput);
         playerRb.AddForce(Vector3.right* speed* horizontalInput);
+
+       
     }
 }
